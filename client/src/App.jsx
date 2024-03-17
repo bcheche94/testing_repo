c30887794b31ff341e41a10da8131f991e11dd3f
bcheche94 <143166864+bcheche94@users.noreply.github.com>
2024-03-17 05:43:16 +0400

    Add files via upload

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import About from './pages/About'
 import Profile from './pages/Profile'
-
+import New_Page_Main_Func from './pages/newPage'
 
 
 export default function App() {
@@ -19,6 +19,7 @@ export default function App() {
       <Route path='/sign-up' element={ <SignUp /> } />
       <Route path='/about' element={ <About /> } />
       <Route path='/profile' element={ <Profile /> } />
+      <Route path='/newpage' element={ <New_Page_Main_Func /> } />
 
     </Routes>
 
